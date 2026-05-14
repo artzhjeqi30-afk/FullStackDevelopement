f4a7a5f6ea4e8143a1adacf7f1ad77151e74b8f9
artzhjeqi30-afk <artzhjeqi30@gmail.com>
2026-05-14 20:24:02 +0200

    Module 26

---
+++ Module 26/part 2/main.js
@@ -0,0 +1,58 @@
+// console.log("Welcome to Digital School")
+
+// var x = 10;
+// var text = "Digital School";
+
+// var a = 50 + 50;
+// console.log(a);
+
+// var b = 20;
+// var c = 30;
+
+// var d = b + c;
+// console.log(d);
+
+// var s = 32;
+// var k = 44;
+// var h = s * k;
+// console.log(h);
+
+// var a = 99;
+// a++;
+// console.log(a);
+// a--;
+// console.log(a);
+
+// var x = 15;
+// x+=5;
+// console.log(x);
+
+// var x = 5;
+// var y = 5;
+
+// console.log(x == y);
+
+// console.log(x < y);
+
+
+// var firstName = "Art";
+// var lastName = "Zhjeqi";
+// var fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+var school = "Digital School";
+console.log(`Hello from ${school}!`);
+
+
+// var age = 17;
+// console.log(age >= 18 && age <= 65);
+
+// var var1 = 5;
+// var var2 = 6;
+
+// console.log(var1 == 5|| var2 == 5);
+
+var var3 = 9;
+var var4 = 10;
+
+console.log(!var3 == var4);
