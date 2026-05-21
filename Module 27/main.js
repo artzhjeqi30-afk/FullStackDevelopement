b6b7c90e3a87c521f4a669f00708ae3a7417f14d
artzhjeqi30-afk <artzhjeqi30@gmail.com>
2026-05-21 20:38:25 +0200

    Module 27

--- Module 27/main.js
+++ Module 27/main.js
@@ -1,30 +1,64 @@
-function showmessage(){
+// function showmessage(){
 
-     alert('This is my first function')
- }
+//      alert('This is my first function')
+//  }
 
- function sum(number1, number2){
-     return number1 + number2
- }
+//  function sum(number1, number2){
+//      return number1 + number2
+//  }
 
-console.log(sum(32, 42))
+// console.log(sum(32, 42))
 
-function toCelcius(Fahrenheit){
-    return (5/9) * (Fahrenheit - 32) 
+// function toCelcius(Fahrenheit){
+//     return (5/9) * (Fahrenheit - 32) 
+// }
+
+//  var result = toCelcius(54)
+//  console.log("Fahrenheit in celcius is  "+result+" degrees")
+
+
+function digitalschool(){
+    var shkolla = 'DigitalSchool'
+    alert(shkolla)
+} 
+
+digitalschool();
+
+function siperfaqja_e_trekendeshit(baza , lartesia){
+    return baza * lartesia * 0.5;
 }
 
- var result = toCelcius(54)
- console.log("Fahrenheit in celcius is  "+result+" degrees")
+console.log('Siperfaqja e trekendeshit eshte: ' + siperfaqja_e_trekendeshit(5, 7));
 
-
-function calculator(nr1, nr2) {
-    return {
-        sum: nr1 + nr2,
-        multiply: nr1 * nr2,
-        subtract: nr1 - nr2,
-        divide: nr1 / nr2
-    };
+var audia = {
+    type : 'A6',
+    color : 'White',
+    viti_i_prodhimit : 2016,
+    karburanti : 'Benzina',
+    engine:function(){
+        alert('Vrommm')
+    }
 }
 
-console.log(calculator(77, 11)); 
+audia.engine();
 
+var grupi_7 = {
+    niveli : 'Web advanced',
+    ora : '19:30',
+    nr_nx : 14
+}
+
+alert(grupi_7.nr_nx)
+
+var personi = {
+    emri : 'Arti',
+    mosha : 17,
+    vendi : 'Podujeve',
+    gjatesia : 1.94,
+    hobbi:function(){
+        alert('Futboll')
+    }
+}
+
+personi.hobbi();
+console.log('Emri im eshte ' + personi.emri + ', une jam ' + personi.mosha + ' vjeq, jam nga ' + personi.vendi + ' dhe gjatesia ime eshte ' + personi.gjatesia + ' metra.')
